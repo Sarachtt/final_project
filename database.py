c3ad5727e4a2bcc74946a9c1e56f128d2ac8a35c
Sarachtt <Sarach.ta@ku.th>
2023-11-28 02:58:57 +0700

    27/11

--- database.py
+++ database.py
@@ -108,3 +108,5 @@ class DB:
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
 
 # modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
+git config --global user.email "Sarach.ta@ku.th"
+git config --global user.name "Sarachtt"
